62c4120365ba52e729f9e34fc85b05e3e838b207
Feliciano <ryanfeliciano.silva@outlook.com>
2022-08-21 11:31:01 -0300

    update

--- a008.py
+++ a008.py
@@ -3,3 +3,16 @@
 #  from math import factorial, sqrt
 
 
+from math import sqrt
+import random
+import emoji
+
+num = int(input("Digite um número: "))
+raiz2 = sqrt(num)
+
+print("A raiz quadrada de {} é igual a {:.2f}".format(num, raiz2))
+
+random = random.randint(1, 10)
+print("O número aleatório gerado foi {}".format(random))
+
+print(emoji.emojize("Olá, Mundo! :earth_americas:", language="alias"))
---
+++ ex016.py
@@ -0,0 +1,4 @@
+from math import floor
+
+var = float(input("Digite um número real: "))
+print("A parte inteira do número {} é {}".format(var, floor(var)))
